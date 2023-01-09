1664be5da5efe151a3e9ec3c0d6675f4a4331437
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-01-09 11:29:44 +0530

    layout created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Document from './Components/Document/Document';
 
 function App() {
   return (
     <div className="App">
+     <div className="column left">
+       
+      </div>
+
+      <div className="column right">
+      <Document/>
+      </div>
      
     </div>
   );
---
+++ src/Components/Document/Document.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Document/Document.css"
+
+export default function Document() {
+  return (
+    <>
+        <div className='container'>
+
+        </div>
+    </>
+  )
+}
