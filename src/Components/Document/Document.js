b84054aca2a64b776aa36944c866ad9460ded29e
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-01-09 16:59:27 +0530

    created input fields

--- src/Components/Document/Document.js
+++ src/Components/Document/Document.js
@@ -5,7 +5,25 @@ export default function Document() {
   return (
     <>
         <div className='container'>
+            <div className='fieldOne'>
+            <h3>Document</h3>
 
+            <label for="name">Pet Name</label>
+            <input type="text" name="name" id="name" />
+
+            <label for="Documentname">Document Name</label>
+            <input type="text" name="Docname" id="DocName" />
+
+            <label for="name">Date</label>
+            <input></input>
+
+            <label for="name">Upload Documents</label>
+            <input></input>
+            </div>
+            <div className='fieldTwo'>
+              <button>Submit</button>
+              <button>Next</button>
+            </div>
         </div>
     </>
   )
